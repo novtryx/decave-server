ec36a63b9e7117a696177560675132032d44bd7d
emmanuelkawekwune <emmanuelkawekwune@punch.agency>
2026-02-02 21:41:04 +0100

    fixed descrepancy

--- src/services/analytics.service.ts
+++ src/services/analytics.service.ts
@@ -257,7 +257,7 @@ export class AnalyticsService {
       {
         $match: {
           $expr: {
-            $eq: ["$ticketInfo.title", "$eventData.tickets.title"],
+            $eq: ["$ticket", "$eventData.tickets._id"],
           },
         },
       },
