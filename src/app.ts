e9f786e497b480600def0c1efa76cfc12714bab0
emmanuelkawekwune <emmanuelkawekwune@punch.agency>
2026-02-12 11:25:55 +0100

    extended limit

--- src/app.ts
+++ src/app.ts
@@ -109,8 +109,8 @@ const corsOptions: cors.CorsOptions = {
 
 app.set("trust proxy", 1);
 app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '150mb' }));
+app.use(express.urlencoded({ limit: '150mb', extended: true }));
 
 // -----------------------------
 // Ensure DB & Redis Connection
